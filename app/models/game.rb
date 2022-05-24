ea2ced4d5ccd9d51b3477f6f2a2c32eaed0a944c
Greg <garnold.805@gmail.com>
2022-05-24 11:25:49 -0400

    Completed Assignment

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,10 @@
 class Game < ActiveRecord::Base
+    has_many :reviews
+
+    # same as has_many :reviews macro
+    
+    # def reviews
+    #   Review.where(game_id: self.id)
+    # end
   
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -1,3 +1,12 @@
 class Review < ActiveRecord::Base
+    belongs_to :game
+
+    # same as belongs_to :game macro
+
+    # # a review belongs to a game
+    # def game
+    #   # self is the review instance
+    #   Game.find(self.game_id)
+    # end
   
-end
+  end
